0fc2cc1e0e05e42a3d798b4b7b51b8bc26849055
varens <stan.svz@gmail.com>
2022-05-18 09:10:04 +0900

    Remove termcolor; set consistent whitespace.

--- PRETty.py
+++ PRETty.py
@@ -1,40 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
 import argparse
 from time import sleep
-from termcolor import colored, cprint
-main_color = 'green'
-sub_color = 'blue'
-line_color = 'red'
-text_color = 'white'
+
 alt_text = ' automation tool'
 
 def main_text():
-	cprint("              \"PRinter Exploitation Toolkit\" LAN"+alt_text+"             ", text_color)
+  print("              \"PRinter Exploitation Toolkit\" LAN"+alt_text+"             ")
 def main_art():
-	cprint(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ", main_color)
-	cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌ ", main_color)
-	cprint("▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀  ▀▀▀▀█░█▀▀▀▀  ▀▀▀▀█░█▀▀▀▀ ▐░▌       ▐░▌ ", main_color)
-	cprint("▐░▌       ▐░▌▐░▌       ▐░▌▐░▌               ▐░▌          ▐░▌     ▐░▌       ▐░▌ ", main_color)
-	cprint("▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌ ", main_color)
-	cprint("▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌     ▐░░░░░░░░░░░▌ ", main_color)
-	cprint("▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░▌      ▀▀▀▀█░█▀▀▀▀  ", main_color)
-	cprint("▐░▌          ▐░▌     ▐░▌  ▐░▌               ▐░▌          ▐░▌          ▐░▌      ", main_color)
-	cprint("▐░▌          ▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌          ▐░▌      ", main_color)
-	cprint("▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌          ▐░▌      ", main_color)
-	cprint(" ▀            ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀            ▀            ▀       ", main_color)
-	cprint("  BusesCanFly   		  			    76 33 2e 30        ", sub_color)
-	cprint("-------------------------------------------------------------------------------", line_color)
-	main_text()
-	cprint("-------------------------------------------------------------------------------", line_color)
+  print("PRETTY")
+  main_text()
+  print("-------------------------------------------------------------------------------")
 def interactive_steps():
-	cprint("Step 1: Generate IP list", 'yellow')
-	cprint("Step 2: Select IP list", 'yellow')
-	cprint("Step 3: Select PRET command input file", 'yellow')
-	cprint("Step 4: Select shell type", 'yellow')
-	cprint("Step 5: Observe all laws and ethical/moral codes :D", 'yellow')
-	cprint("Step 6: >:)\n", 'yellow')
+  print("Step 1: Generate IP list")
+  print("Step 2: Select IP list")
+  print("Step 3: Select PRET command input file")
+  print("Step 4: Select shell type")
+  print("Step 5: Observe all laws and ethical/moral codes :D")
+  print("Step 6: >:)\n")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cli', dest='cli', action='store_true',
@@ -50,105 +34,105 @@ args = parser.parse_args()
 
 sleep_time=1.5
 def PrinterLogSort():
-	os.system('tshark -r ./IP/scan.pcap > ./IP/pcap.txt 2>/dev/null')
-	os.system('cat ./IP/pcap.txt | grep -iE "Hewlett|Brother|Kyocera|Laserjet" > ./IP/raw_list 2>/dev/null')
-	os.system('awk \'{print $8}\' ./IP/raw_list > ./IP/Printer_list')
-	sleep(0.5)
-	cprint('Successfully processed raw data', 'green')
-	os.system('rm -rf ./IP/scan.pcap && rm -rf ./IP/pcap.txt && rm -rf ./IP/raw_list 2>/dev/null')
-	sleep(sleep_time)
-	cprint('Cleaned raw data', 'green')
-	sleep(sleep_time)
-	cprint('\nLocated '+ str(sum(1 for line in open ('./IP/Printer_list'))) +' printers, storing as ./IP/Printer_list\n', 'yellow')
+  os.system('tshark -r ./IP/scan.pcap > ./IP/pcap.txt 2>/dev/null')
+  os.system('cat ./IP/pcap.txt | grep -iE "Hewlett|Brother|Kyocera|Laserjet" > ./IP/raw_list 2>/dev/null')
+  os.system('awk \'{print $8}\' ./IP/raw_list > ./IP/Printer_list')
+  sleep(0.5)
+  print('Successfully processed raw data')
+  os.system('rm -rf ./IP/scan.pcap && rm -rf ./IP/pcap.txt && rm -rf ./IP/raw_list 2>/dev/null')
+  sleep(sleep_time)
+  print('Cleaned raw data')
+  sleep(sleep_time)
+  print('\nLocated '+ str(sum(1 for line in open ('./IP/Printer_list'))) +' printers, storing as ./IP/Printer_list\n')
 
 def PRETty_Interactive():
-	gen_new= str(input("Generate new IP list? [y/N] "))
-	if gen_new == 'y':
+  gen_new= str(input("Generate new IP list? [y/N] "))
+  if gen_new == 'y':
 
-		set_ip_range = input("Set IP range for scanning? [y/N] ")
-		if set_ip_range == 'y':
-			ip_range = str(input("IP range: [ex. 192.168.0.0/16] "))
-		else:
-			ip_range = '--localnet'
+    set_ip_range = input("Set IP range for scanning? [y/N] ")
+    if set_ip_range == 'y':
+      ip_range = str(input("IP range: [ex. 192.168.0.0/16] "))
+    else:
+      ip_range = '--localnet'
 
-		cprint("ARP scanning LAN for devices...", 'green')
-		sleep(1.5)
-		os.system('sudo arp-scan -g '+ip_range+' -W ./IP/scan.pcap')
-		cprint('Successfully collected IP\'s', 'green')
+    print("ARP scanning LAN for devices...")
+    sleep(1.5)
+    os.system('sudo arp-scan -g '+ip_range+' -W ./IP/scan.pcap')
+    print('Successfully collected IP\'s')
 
-		PrinterLogSort()
+    PrinterLogSort()
 
-	list_answer = str(input("Use default IP list? [Y/n] "))
-	if list_answer == 'n':
-		cprint('An example IP list can be found at ./IP/example', 'green')
-		cprint('Available IP lists: ', 'green')
-		os.system('ls ./IP/')
-		print('\n')
-		list = './IP/' + str(input("Which list? ./IP/"))
-		cprint('\nLoaded '+ str(sum(1 for line in open (list))) +' IP\'s\n', 'yellow')
-	else:
-		cprint('Using "./IP/Printer_list" as IP range', 'green')
-		list = './IP/Printer_list'
-		cprint('\nLoaded '+ str(sum(1 for line in open ('./IP/Printer_list'))) +' IP\'s\n', 'yellow')
+  list_answer = str(input("Use default IP list? [Y/n] "))
+  if list_answer == 'n':
+    print('An example IP list can be found at ./IP/example')
+    print('Available IP lists: ')
+    os.system('ls ./IP/')
+    print('\n')
+    list = './IP/' + str(input("Which list? ./IP/"))
+    print('\nLoaded '+ str(sum(1 for line in open (list))) +' IP\'s\n')
+  else:
+    print('Using "./IP/Printer_list" as IP range')
+    list = './IP/Printer_list'
+    print('\nLoaded '+ str(sum(1 for line in open ('./IP/Printer_list'))) +' IP\'s\n')
 
-	commands_list = str(input("Use default ./commands/pret_pagecount.txt command file? [Y/n] "))
-	if commands_list == 'n':
-		cprint('Example command lists: (./commands)', 'green')
-		os.system('ls ./commands/')
-		print('\n')
-		commands_list = './commands/' + str(input("Which command list? "+'./commands/'))
-		cprint('Commands: ', 'green')
-		os.system('cat '+commands_list)
-		print('\n')
-	else:
-		cprint('Using "./commands/pret_pagecount.txt" as PRET commands', 'green')
-		commands_list = './commands/pret_pagecount.txt'
-		cprint('Commands: ', 'yellow')
-		os.system('cat ./commands/pret_pagecount.txt')
-		print('\n')
+  commands_list = str(input("Use default ./commands/pret_pagecount.txt command file? [Y/n] "))
+  if commands_list == 'n':
+    print('Example command lists: (./commands)')
+    os.system('ls ./commands/')
+    print('\n')
+    commands_list = './commands/' + str(input("Which command list? "+'./commands/'))
+    print('Commands: ')
+    os.system('cat '+commands_list)
+    print('\n')
+  else:
+    print('Using "./commands/pret_pagecount.txt" as PRET commands')
+    commands_list = './commands/pret_pagecount.txt'
+    print('Commands: ')
+    os.system('cat ./commands/pret_pagecount.txt')
+    print('\n')
 
-	shell_type = input("Shell Type: [ps, pjl, pcl] ")
+  shell_type = input("Shell Type: [ps, pjl, pcl] ")
 
-	debug = input('Enable PRET debug mode? [y/N] ')
-	if debug == 'y':
-		debug_enabled = '-d'
-	else:
-		debug_enabled = ''
-	print('')
+  debug = input('Enable PRET debug mode? [y/N] ')
+  if debug == 'y':
+    debug_enabled = '-d'
+  else:
+    debug_enabled = ''
+  print('')
 
-	with open(list) as inf:
-		lines = [line.strip() for line in inf]
+  with open(list) as inf:
+    lines = [line.strip() for line in inf]
 
-	i=0
-	while i < len(lines):
-		os.system('../pret.py '+debug_enabled+' -i '+commands_list+' -q '+lines[i]+' '+ shell_type)
-		i+=1
+  i=0
+  while i < len(lines):
+    os.system('../pret.py '+debug_enabled+' -i '+commands_list+' -q '+lines[i]+' '+ shell_type)
+    i+=1
 
 def PRETty_cli():
-	os.system('sudo arp-scan -g '+args.ip_range+' -W ./IP/scan.pcap')
-	PrinterLogSort()
-	sleep(1)
-	list = './IP/Printer_list'
-	with open(list) as inf:
-		lines = [line.strip() for line in inf]
-	i=0
-	while i < len(lines):
-		os.system('../pret.py -i ./commands/'+args.commands_list+' -q '+lines[i]+' '+ args.shell_type)
-		i+=1
+  os.system('sudo arp-scan -g '+args.ip_range+' -W ./IP/scan.pcap')
+  PrinterLogSort()
+  sleep(1)
+  list = './IP/Printer_list'
+  with open(list) as inf:
+    lines = [line.strip() for line in inf]
+  i=0
+  while i < len(lines):
+    os.system('../pret.py -i ./commands/'+args.commands_list+' -q '+lines[i]+' '+ args.shell_type)
+    i+=1
 
 if args.cli:
-	main_color = 'red'
-	sub_color = 'green'
-	line_color = 'white'
-	text_color = 'yellow'
-	alt_text =' AUTOPWN      '
-	main_art()
-	sleep_time=0.5
-	PRETty_cli()
+  main_color = 'red'
+  sub_color = 'green'
+  line_color = 'white'
+  text_color = 'yellow'
+  alt_text =' AUTOPWN      '
+  main_art()
+  sleep_time=0.5
+  PRETty_cli()
 else:
-	alt_text = ' automation tool'
-	main_art()
-	interactive_steps()
-	sleep_time=1.5
-	PRETty_Interactive()
+  alt_text = ' automation tool'
+  main_art()
+  interactive_steps()
+  sleep_time=1.5
+  PRETty_Interactive()
 
